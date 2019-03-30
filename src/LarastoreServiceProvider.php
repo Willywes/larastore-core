8b51a224db3b5d7bf0a2965b0bf96cc0f82af52e
willywes <willywes@hotmail.cl>
2019-03-29 23:20:44 -0300

    Initial

---
+++ src/LarastoreServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Willywes\Larastore;
+
+use Illuminate\Support\ServiceProvider;
+use PhpParser\Node\Stmt\Label;
+
+class LarastoreServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+    }
+
+    public function register()
+    {
+    }
+}
+?>
